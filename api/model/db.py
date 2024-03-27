8f6611e7125397e76972abf6e21d20f415882c50
Janiel <janjannunez777@gmail.com>
2024-03-26 22:33:27 -0400

    Refactoring - instead of using csv a .env is now used to read the credentials in the db.py file
    This is for the purpose of making it easier to configure the environment variables in Heroku when bringing the project to production.

--- api/model/db.py
+++ api/model/db.py
@@ -2,19 +2,24 @@ import configparser
 import csv
 import psycopg2
 import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
 
 class Database:
     def __init__(self):
-        self.credentials = self.connect_db()
+        # self.credentials = self.connect_db()
         self.conexion = psycopg2.connect(
-            host=self.credentials['host'],
-            user=self.credentials['user'],
-            password=self.credentials['password'],
-            port=self.credentials['port'],
-            database=self.credentials['database'],
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            port=os.getenv('DB_PORT'),
         )
 
-    def connect_db(self):
+    # def connect_db(self):
         # current_dir = os.path.dirname(os.path.abspath(__file__))
         # credentials_path = os.path.join(current_dir, 'credentials.csv')
 
@@ -25,9 +30,9 @@ class Database:
         #     db_dict = {'host':host, 'user':user, 'password':password, 'port':port, 'database':db}
         #     return db_dict
 
-        db_dict = {'host':os.getenv('HOST'), 'user':os.getenv('USER'), 'password':os.getenv('PASSWORD'), 'port':os.getenv('PORT'), 'database':os.getenv('DATABASE')
-        }
-        return db_dict
+        # db_dict = {'host':os.getenv('HOST'), 'user':os.getenv('USER'), 'password':os.getenv('PASSWORD'), 'port':os.getenv('PORT'), 'database':os.getenv('DATABASE')
+        # }
+        # return db_dict
 
     def close(self):
         self.conexion.close()
