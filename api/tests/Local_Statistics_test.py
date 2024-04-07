70c48fb3e4cb16d2387e089768684425f7c7eafa
Janiel <janjannunez777@gmail.com>
2024-04-06 22:20:57 -0400

    Logic implemented so that employees can or cannot access certain statistics.

--- api/tests/Local_Statistics_test.py
+++ api/tests/Local_Statistics_test.py
@@ -2,46 +2,136 @@
 
 def test_get_top_paid_regular_employees_by_hotel(client):
     for hid in range(1,41):
-        response = client.get(f'/hotel/{hid}/highestpaid')
-        assert response.status_code == 200, f"El código de respuesta debe ser 200, pero se obtuvo {response.status_code}"
+        body = {
+            "eid": 1
+        }
+        response = client.get(f'/hotel/{hid}/highestpaid', json=body)
+        assert response.status_code == 200 or \
+        response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel {hid}.",\
+        f"El código de respuesta debe ser 200, pero se obtuvo {response.status_code}"
 
         data = response.get_json()
-        assert isinstance(data, list), "Los datos deben ser una lista"
-        assert len(data) <= 3, "La lista no debe contener más de 3 empleados"
+        if(isinstance(data, list)):
+            assert isinstance(data, list), "Los datos deben ser una lista"
+            assert len(data) <= 3, "La lista no debe contener más de 3 empleados"
+            if data:
+                for employee in data:
+                    assert 'age' in employee, "'age' debe estar presente en cada empleado"
+                    assert 'eid' in employee, "'eid' debe estar presente en cada empleado"
+                    assert 'fname' in employee, "'fname' debe estar presente en cada empleado"
+                    assert 'lname' in employee, "'lname' debe estar presente en cada empleado"
+                    assert 'position' in employee and employee['position'] == 'Regular', "Todos los empleados deben ser 'Regular'"
+                    assert 'salary' in employee, "'salary' debe estar presente en cada empleado"
+                    assert 'hid' in employee and employee['hid'] == hid, f"'hid' debe ser {hid} en cada empleado"
+                    # Verifica que los salarios estén en orden descendente
+                    salaries = [emp['salary'] for emp in data]
+                    assert salaries == sorted(salaries, reverse=True), "Los salarios deben estar en orden descendente"
 
-        if data:
-            for employee in data:
-                assert 'age' in employee, "'age' debe estar presente en cada empleado"
-                assert 'eid' in employee, "'eid' debe estar presente en cada empleado"
-                assert 'fname' in employee, "'fname' debe estar presente en cada empleado"
-                assert 'lname' in employee, "'lname' debe estar presente en cada empleado"
-                assert 'position' in employee and employee['position'] == 'Regular', "Todos los empleados deben ser 'Regular'"
-                assert 'salary' in employee, "'salary' debe estar presente en cada empleado"
-                assert 'hid' in employee and employee['hid'] == hid, f"'hid' debe ser {hid} en cada empleado"
-                # Verifica que los salarios estén en orden descendente
-                salaries = [emp['salary'] for emp in data]
-                assert salaries == sorted(salaries, reverse=True), "Los salarios deben estar en orden descendente"
+    body = {
+        "eid": 2#Regular
+    }
+    response = client.get(f'/hotel/{1}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{3}/highestpaid', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel {3}.", \
+        f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{9}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{10}/highestpaid', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel 10.", \
+        f"El empleado 2 no puede ver las estadisticas del hotel 10"
+
+    body = {
+        "eid": 3 #Administrator
+    }
+    response = client.get(f'/hotel/{10}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 10"
+    response = client.get(f'/hotel/{1}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+    response = client.get(f'/hotel/{2}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 2"
+    response = client.get(f'/hotel/{20}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 20"
+    body = {
+        "eid": 4 #Supervisor
+    }
+    response = client.get(f'/hotel/{1}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado {body['eid']} no puede ver las estadisticas del hotel 1"
+    response = client.get(f'/hotel/{2}/highestpaid', json=body)
+    assert response.status_code == 200, f"El empleado {body['eid']} no puede ver las estadisticas del hotel 2"
+    response = client.get(f'/hotel/{3}/highestpaid', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel 3.", \
+        f"El empleado {body['eid']} no puede ver las estadisticas del hotel 3"
+
 
 
 def test_get_top5_credit_card_reservations_by_hotel(client):
     for hid in range(1, 41):  # Asumiendo que quieres probar para hoteles con ID del 1 al 40
-        response = client.get(f'/hotel/{hid}/mostcreditcard')
-        assert response.status_code == 200, f"El código de respuesta debe ser 200 para el hotel {hid}, pero se obtuvo {response.status_code}"
+        body = {
+            "eid": 1
+        }
+        response = client.get(f'/hotel/{hid}/mostcreditcard', json=body)
+        assert response.status_code == 200 or \
+        response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel {hid}.",\
+        f"El código de respuesta debe ser 200 para el hotel {hid}, pero se obtuvo {response.status_code}"
 
         data = response.get_json()
-        assert isinstance(data, list), f"Los datos deben ser una lista para el hotel {hid}"
-        assert len(data) <= 5, f"La lista no debe contener más de 5 clientes para el hotel {hid}"
+        if isinstance(data, list):
+            assert isinstance(data, list), f"Los datos deben ser una lista para el hotel {hid}"
+            assert len(data) <= 5, f"La lista no debe contener más de 5 clientes para el hotel {hid}"
 
-        if data:
-            for c in data:
-                assert 'clid' in c, f"'clid' debe estar presente en cada cliente para el hotel {hid}"
-                assert 'fname' in c, f"'fname' debe estar presente en cada cliente para el hotel {hid}"
-                assert 'lname' in c, f"'lname' debe estar presente en cada cliente para el hotel {hid}"
-                assert 'reservation_count' in c, f"'reservation_count' debe estar presente en cada cliente para el hotel {hid}"
-                assert isinstance(c['reservation_count'],
-                                  int), f"'reservation_count' debe ser un entero para el cliente {c['clid']} en el hotel {hid}"
+            if data:
+                for c in data:
+                    assert 'clid' in c, f"'clid' debe estar presente en cada cliente para el hotel {hid}"
+                    assert 'fname' in c, f"'fname' debe estar presente en cada cliente para el hotel {hid}"
+                    assert 'lname' in c, f"'lname' debe estar presente en cada cliente para el hotel {hid}"
+                    assert 'reservation_count' in c, f"'reservation_count' debe estar presente en cada cliente para el hotel {hid}"
+                    assert isinstance(c['reservation_count'],
+                                      int), f"'reservation_count' debe ser un entero para el cliente {c['clid']} en el hotel {hid}"
 
-                # Extraer los conteos de reservaciones de los clientes
-                reservation_counts = [c['reservation_count'] for c in data]
-                # Verificar que los conteos de reservaciones estén en orden descendente
-                assert reservation_counts == sorted(reservation_counts,reverse=True), "Los conteos de reservaciones deben estar en orden descendente para el hotel {hid}"
+                    # Extraer los conteos de reservaciones de los clientes
+                    reservation_counts = [c['reservation_count'] for c in data]
+                    # Verificar que los conteos de reservaciones estén en orden descendente
+                    assert reservation_counts == sorted(reservation_counts,reverse=True), "Los conteos de reservaciones deben estar en orden descendente para el hotel {hid}"
+
+    body = {
+        "eid": 2
+    }
+    response = client.get(f'/hotel/{1}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{3}/mostcreditcard', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel {3}.", \
+        f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{9}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+
+    response = client.get(f'/hotel/{10}/mostcreditcard', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel 10.", \
+        f"El empleado 2 no puede ver las estadisticas del hotel 10"
+
+    body = {
+        "eid": 3
+    }
+    response = client.get(f'/hotel/{10}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 10"
+    response = client.get(f'/hotel/{1}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 1"
+    response = client.get(f'/hotel/{2}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 2"
+    response = client.get(f'/hotel/{20}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado 2 no puede ver las estadisticas del hotel 20"
+    body = {
+        "eid": 4
+    }
+    response = client.get(f'/hotel/{1}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado {body['eid']} no puede ver las estadisticas del hotel 1"
+    response = client.get(f'/hotel/{2}/mostcreditcard', json=body)
+    assert response.status_code == 200, f"El empleado {body['eid']} no puede ver las estadisticas del hotel 2"
+    response = client.get(f'/hotel/{3}/mostcreditcard', json=body)
+    assert response.get_json() == f"El empleado {body['eid']} no tiene acceso a las estadísticas del hotel 3.",\
+    f"El empleado {body['eid']} no puede ver las estadisticas del hotel 3"
