8e59dd71f035f887161a7e0efd614233ed3c64e3
Janiel <janjannunez777@gmail.com>
2024-05-02 16:00:45 -0400

    Updated 4 plots to use plotly.express instead of matplotlib

--- front-end/GlobalStatsPlots.py
+++ front-end/GlobalStatsPlots.py
@@ -6,76 +6,108 @@ class GlobalStatsPlots:
     def __init__(self):
         pass
 
+    # def mostrar_graficos_most_reservation(self, session):
+    #     url = 'http://127.0.0.1:5000/most/reservation'
+    #     data = {
+    #         "eid": 3
+    #     }
+    #     response = session.get(url, json=data, verify=False)
+    #     # Verifica si la solicitud fue exitosa
+    #     if response.status_code == 200:
+    #         # print("Respuesta exitosa")
+    #         data_json = response.json()
+    #         # Convierte los datos JSON en un DataFrame de pandas
+    #         df = pd.DataFrame(data_json)
+    #
+    #         # Configura el tamaño de la figura
+    #         plt.figure(figsize=(10, 6))
+    #
+    #         # Crea un gráfico de barras
+    #         bars = plt.bar(df['hname'], df['reservation_count'], color='skyblue')
+    #
+    #         # Añade título y etiquetas a los ejes
+    #         plt.title('Top 10% de Hoteles con Más Reservaciones')
+    #         plt.xlabel('Nombre del Hotel')
+    #         plt.ylabel('Número de Reservaciones')
+    #
+    #         # Rotar las etiquetas del eje X para mejor legibilidad
+    #         plt.xticks(rotation=45, ha="right")
+    #
+    #         # Añade los valores exactos encima de las barras
+    #         for bar in bars:
+    #             height = bar.get_height()
+    #             plt.text(bar.get_x() + bar.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
+    #
+    #         # Ajusta el layout para asegurar que las etiquetas no se corten
+    #         plt.tight_layout()
+    #
+    #         # Muestra el gráfico
+    #         plt.show()
+    #     else:
+    #         pass
+    #         # print("Error en la respuesta:", response.status_code)
+
     def mostrar_graficos_most_reservation(self, session):
         url = 'http://127.0.0.1:5000/most/reservation'
-        data = {
-            "eid": 3
-        }
+        data = {"eid": 3}
         response = session.get(url, json=data, verify=False)
-        # Verifica si la solicitud fue exitosa
         if response.status_code == 200:
-            # print("Respuesta exitosa")
             data_json = response.json()
-            # Convierte los datos JSON en un DataFrame de pandas
             df = pd.DataFrame(data_json)
-
-            # Configura el tamaño de la figura
-            plt.figure(figsize=(10, 6))
-
-            # Crea un gráfico de barras
-            bars = plt.bar(df['hname'], df['reservation_count'], color='skyblue')
-
-            # Añade título y etiquetas a los ejes
-            plt.title('Top 10% de Hoteles con Más Reservaciones')
-            plt.xlabel('Nombre del Hotel')
-            plt.ylabel('Número de Reservaciones')
-
-            # Rotar las etiquetas del eje X para mejor legibilidad
-            plt.xticks(rotation=45, ha="right")
-
-            # Añade los valores exactos encima de las barras
-            for bar in bars:
-                height = bar.get_height()
-                plt.text(bar.get_x() + bar.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
-
-            # Ajusta el layout para asegurar que las etiquetas no se corten
-            plt.tight_layout()
-
-            # Muestra el gráfico
-            plt.show()
+            fig = px.bar(df, x='hname', y='reservation_count', title='Top 10% de Hoteles con Más Reservaciones',
+                         labels={'hname': 'Nombre del Hotel', 'reservation_count': 'Número de Reservaciones'},
+                         color='reservation_count', color_continuous_scale=px.colors.sequential.Viridis)
+            fig.update_traces(texttemplate='%{y}', textposition='outside')
+            fig.update_layout(autosize=False, width=900, height=600)
+            fig.show()
         else:
-            pass
-            # print("Error en la respuesta:", response.status_code)
+            print("Error en la respuesta:", response.status_code)
 
+
+    # def graficar_capacidad_hoteles(self, session):
+    #     url = 'http://127.0.0.1:5000/most/capacity'
+    #     data = {
+    #         "eid": 3
+    #     }
+    #     response = session.get(url, json=data, verify=False)
+    #     # Verifica si la solicitud fue exitosa
+    #     if response.status_code == 200:
+    #         # print("Respuesta exitosa")
+    #         data_json = response.json()
+    #         # Convierte los datos JSON en un DataFrame de pandas
+    #         df = pd.DataFrame(data_json)
+    #
+    #         # global_statistic_widget.clear_output(wait=True)  # Limpiamos la salida anterior
+    #         plt.figure(figsize=(10, 6))  # Tamaño del gráfico
+    #         bars = plt.bar(df['hname'], df['total_capacity'], color='teal')  # Gráfico de barras
+    #
+    #         # Añadimos etiquetas en las barras
+    #         for bar in bars:
+    #             yval = bar.get_height()
+    #             plt.text(bar.get_x() + bar.get_width() / 2, yval + 1, int(yval), ha='center', va='bottom')
+    #
+    #         plt.title('Top 5 Hoteles con Mayor Capacidad de Clientes')  # Título del gráfico
+    #         plt.xlabel('Nombre del Hotel')  # Etiqueta eje X
+    #         plt.ylabel('Capacidad Total')  # Etiqueta eje Y
+    #         plt.xticks(rotation=45, ha='right')  # Rotamos las etiquetas para que sean legibles
+    #         plt.tight_layout()  # Ajuste del layout
+    #         plt.show()
 
     def graficar_capacidad_hoteles(self, session):
         url = 'http://127.0.0.1:5000/most/capacity'
-        data = {
-            "eid": 3
-        }
+        data = {"eid": 3}
         response = session.get(url, json=data, verify=False)
-        # Verifica si la solicitud fue exitosa
         if response.status_code == 200:
-            # print("Respuesta exitosa")
             data_json = response.json()
-            # Convierte los datos JSON en un DataFrame de pandas
             df = pd.DataFrame(data_json)
-
-            # global_statistic_widget.clear_output(wait=True)  # Limpiamos la salida anterior
-            plt.figure(figsize=(10, 6))  # Tamaño del gráfico
-            bars = plt.bar(df['hname'], df['total_capacity'], color='teal')  # Gráfico de barras
-
-            # Añadimos etiquetas en las barras
-            for bar in bars:
-                yval = bar.get_height()
-                plt.text(bar.get_x() + bar.get_width() / 2, yval + 1, int(yval), ha='center', va='bottom')
-
-            plt.title('Top 5 Hoteles con Mayor Capacidad de Clientes')  # Título del gráfico
-            plt.xlabel('Nombre del Hotel')  # Etiqueta eje X
-            plt.ylabel('Capacidad Total')  # Etiqueta eje Y
-            plt.xticks(rotation=45, ha='right')  # Rotamos las etiquetas para que sean legibles
-            plt.tight_layout()  # Ajuste del layout
-            plt.show()
+            fig = px.bar(df, x='hname', y='total_capacity', title='Top 5 Hoteles con Mayor Capacidad de Clientes',
+                         labels={'hname': 'Nombre del Hotel', 'total_capacity': 'Capacidad Total'},
+                         color='total_capacity', color_continuous_scale=px.colors.sequential.Plasma)
+            fig.update_traces(texttemplate='%{y}', textposition='outside')
+            fig.update_layout(autosize=False, width=900, height=600)
+            fig.show()
+        else:
+            print("Error en la respuesta:", response.status_code)
 
     def graficar_payment_method(self, session):
         url = 'http://127.0.0.1:5000/paymentmethod'
--- front-end/LocalStatsPlots.py
+++ front-end/LocalStatsPlots.py
@@ -7,66 +7,97 @@ class LocalStatsPlots:
     def __init__(self):
         pass
 
+    # def graficar_top_3_highest_pais_regular_employees(self, session, json, hid):
+    #     # Endpoint y cuerpo de la petición
+    #     url = f'http://127.0.0.1:5000/hotel/{hid}/highestpaid'
+    #
+    #     # Realizamos la petición al endpoint
+    #     respuesta = session.get(url, json=json)
+    #
+    #     # Verificamos si la petición fue exitosa
+    #     if respuesta.status_code == 200:
+    #         datos = respuesta.json()
+    #
+    #         # Convertimos los datos a DataFrame
+    #         df = pd.DataFrame(datos)
+    #
+    #         # Creamos la gráfica
+    #         plt.figure(figsize=(10, 6))
+    #         bars = plt.barh(df['fname'] + ' ' + df['lname'], df['salary'], color='skyblue')
+    #         plt.xlabel('Salario')
+    #         plt.title('Top 3 Empleados Regulares Mejor Pagados')
+    #
+    #         for bar in bars:
+    #             width = bar.get_width()
+    #             plt.text(width, bar.get_y() + bar.get_height() / 2, f'{width:.2f}', va='center')
+    #
+    #         plt.tight_layout()
+    #
+    #         # Mostramos la gráfica
+    #         plt.show()
+    #     else:
+    #         print("Error al obtener los datos")
+
     def graficar_top_3_highest_pais_regular_employees(self, session, json, hid):
-        # Endpoint y cuerpo de la petición
         url = f'http://127.0.0.1:5000/hotel/{hid}/highestpaid'
-
-        # Realizamos la petición al endpoint
         respuesta = session.get(url, json=json)
-
-        # Verificamos si la petición fue exitosa
         if respuesta.status_code == 200:
             datos = respuesta.json()
-
-            # Convertimos los datos a DataFrame
             df = pd.DataFrame(datos)
-
-            # Creamos la gráfica
-            plt.figure(figsize=(10, 6))
-            bars = plt.barh(df['fname'] + ' ' + df['lname'], df['salary'], color='skyblue')
-            plt.xlabel('Salario')
-            plt.title('Top 3 Empleados Regulares Mejor Pagados')
-
-            for bar in bars:
-                width = bar.get_width()
-                plt.text(width, bar.get_y() + bar.get_height() / 2, f'{width:.2f}', va='center')
-
-            plt.tight_layout()
-
-            # Mostramos la gráfica
-            plt.show()
+            fig = px.bar(df, x='salary', y=df['fname'] + ' ' + df['lname'], orientation='h',
+                         title='Top 3 Empleados Regulares Mejor Pagados', labels={'x': 'Salario', 'y': 'Nombre'},
+                         color='salary', color_continuous_scale=px.colors.sequential.Viridis)
+            fig.update_traces(texttemplate='%{x:.2f}', textposition='outside')
+            fig.update_layout(autosize=False, width=900, height=600)
+            fig.show()
         else:
             print("Error al obtener los datos")
 
+    # def graficar_top_5_clientes_jovenes_con_mas_reservaciones(self, session, hid, json):
+    #     # Suponiendo que puedes pasar eid como un parámetro en la URL
+    #     url = f'http://127.0.0.1:5000/hotel/{hid}/mostcreditcard'
+    #
+    #     # Realizamos la petición al endpoint
+    #     respuesta = session.get(url, json=json)
+    #
+    #     # Verificamos si la petición fue exitosa
+    #     if respuesta.status_code == 200:
+    #         datos = respuesta.json()
+    #
+    #         # Convertimos los datos a DataFrame
+    #         df = pd.DataFrame(datos)
+    #
+    #         # Creamos la gráfica
+    #         plt.figure(figsize=(10, 6))
+    #         plt.barh(df['fname'] + ' ' + df['lname'], df['reservation_count'], color='teal')
+    #         plt.xlabel('Cantidad de Reservaciones')
+    #         plt.title('Top 5 Clientes Menores de 30 Años con Más Reservaciones con Tarjeta de Crédito')
+    #
+    #         # Añadiendo los valores exactos en cada barra
+    #         for bar in plt.barh(df['fname'] + ' ' + df['lname'], df['reservation_count']):
+    #             width = bar.get_width()
+    #             plt.text(width, bar.get_y() + bar.get_height() / 2, f'{int(width)}', va='center')
+    #
+    #         plt.tight_layout()
+    #
+    #         # Mostramos la gráfica
+    #         plt.show()
+    #     else:
+    #         print("Error al obtener los datos")
+
     def graficar_top_5_clientes_jovenes_con_mas_reservaciones(self, session, hid, json):
-        # Suponiendo que puedes pasar eid como un parámetro en la URL
         url = f'http://127.0.0.1:5000/hotel/{hid}/mostcreditcard'
-
-        # Realizamos la petición al endpoint
         respuesta = session.get(url, json=json)
-
-        # Verificamos si la petición fue exitosa
         if respuesta.status_code == 200:
             datos = respuesta.json()
-
-            # Convertimos los datos a DataFrame
             df = pd.DataFrame(datos)
-
-            # Creamos la gráfica
-            plt.figure(figsize=(10, 6))
-            plt.barh(df['fname'] + ' ' + df['lname'], df['reservation_count'], color='teal')
-            plt.xlabel('Cantidad de Reservaciones')
-            plt.title('Top 5 Clientes Menores de 30 Años con Más Reservaciones con Tarjeta de Crédito')
-
-            # Añadiendo los valores exactos en cada barra
-            for bar in plt.barh(df['fname'] + ' ' + df['lname'], df['reservation_count']):
-                width = bar.get_width()
-                plt.text(width, bar.get_y() + bar.get_height() / 2, f'{int(width)}', va='center')
-
-            plt.tight_layout()
-
-            # Mostramos la gráfica
-            plt.show()
+            fig = px.bar(df, x='reservation_count', y=df['fname'] + ' ' + df['lname'], orientation='h',
+                         title='Top 5 Clientes Menores de 30 Años con Más Reservaciones con Tarjeta de Crédito',
+                         labels={'x': 'Cantidad de Reservaciones', 'y': 'Nombre'},
+                         color='reservation_count', color_continuous_scale=px.colors.sequential.Teal)
+            fig.update_traces(texttemplate='%{x:.2f}', textposition='outside')
+            fig.update_layout(autosize=False, width=900, height=600)
+            fig.show()
         else:
             print("Error al obtener los datos")
 
